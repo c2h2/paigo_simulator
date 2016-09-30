fa9bc72f2176930d68fbabe1f8057608c1f99720
c2h2 <yiling.cao@gmail.com>
2016-10-01 02:37:19 +0800

    add trajectory drawing.

--- app.py
+++ app.py
@@ -15,15 +15,15 @@ diff_ms=0
 
 pygame.init()
 clock = pygame.time.Clock()
-width = 800
-height =800
+screen_width = 800
+screen_height =800
 
-screen = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 font = pygame.font.SysFont('Arial', 16)
 black = (0,0,0)
 
 '''constatnt'''
-alex_width = 0.098
+alex_screen_width = 0.098
 object_mass = 1.0
 pwm_acc_rate = 1/500.0
 
@@ -73,9 +73,9 @@ more physics at _update_physics
 
 
 def init():
-    global pos_x, pos_y, width, height, resistance_l, resistance_r
-    #pos_x = width/2.0
-    #pos_y = height/2.0
+    global pos_x, pos_y, screen_width, screen_height, resistance_l, resistance_r
+    #pos_x = screen_width/2.0
+    #pos_y = screen_height/2.0
 
 
 def control_pwm():
@@ -135,10 +135,10 @@ def _update_bot_speed():
 
 
 def _update_physics():
-    global diff_ms, alex_width, pos_x, pos_y, pos_theta, icc_r, icc_omega
+    global diff_ms, alex_screen_width, pos_x, pos_y, pos_theta, icc_r, icc_omega
     diff_s = diff_ms / 1000.0
-    icc_r = (alex_width/2.0) * (speed_l + speed_r) / (speed_r - speed_l + 0.00000001) 
-    icc_omega = (speed_r - speed_l) / alex_width
+    icc_r = (alex_screen_width/2.0) * (speed_l + speed_r) / (speed_r - speed_l + 0.00000001) 
+    icc_omega = (speed_r - speed_l) / alex_screen_width
     avg_speed = (speed_l + speed_r) / 2 
     pos_theta += icc_omega * diff_s
     pos_theta = pos_theta % (2* math.pi)
@@ -150,47 +150,55 @@ def _clear_screen():
     screen.fill((255,255,255))
 
 def _update_text():
-    global minutes, seconds, ms, width, diff_ms, pos_theta, pwm_l, pwm_r,speed_r,speed_l, icc_omega, icc_r
+    global minutes, seconds, ms, screen_width, diff_ms, pos_theta, pwm_l, pwm_r,speed_r,speed_l, icc_omega, icc_r
     y_text_pos = 5
     text ="uptime: {}m{}s {}\"".format(minutes, seconds, ms)
-    screen.blit(font.render(text, True, black), (width - 100, y_text_pos))
+    screen.blit(font.render(text, True, black), (screen_width - 100, y_text_pos))
     y_text_pos+=10
-    screen.blit(font.render("diff: {} ms".format(diff_ms), True, black), (width - 150, y_text_pos))
+    screen.blit(font.render("diff: {} ms".format(diff_ms), True, black), (screen_width - 150, y_text_pos))
     y_text_pos+=10
-    screen.blit(font.render("req_speed_l: {} m/s".format(required_speed_l), True, black), (width - 150, y_text_pos))
+    screen.blit(font.render("req_speed_l: {} m/s".format(required_speed_l), True, black), (screen_width - 150, y_text_pos))
     y_text_pos+=10
-    screen.blit(font.render("req_speed_r: {} m/s".format(required_speed_r), True, black), (width - 150, y_text_pos))
+    screen.blit(font.render("req_speed_r: {} m/s".format(required_speed_r), True, black), (screen_width - 150, y_text_pos))
     y_text_pos+=10
-    screen.blit(font.render("pos_x: {} m".format(round(pos_x,3)), True, black), (width - 150, y_text_pos))
+    screen.blit(font.render("pos_x: {} m".format(round(pos_x,3)), True, black), (screen_width - 150, y_text_pos))
     y_text_pos+=10
-    screen.blit(font.render("pos_y: {} m".format(round(pos_y,3)), True, black), (width - 150, y_text_pos))
+    screen.blit(font.render("pos_y: {} m".format(round(pos_y,3)), True, black), (screen_width - 150, y_text_pos))
     y_text_pos+=10
-    screen.blit(font.render("pos_theta: {} rad".format(round(pos_theta,3)), True, black), (width - 150, y_text_pos))
+    screen.blit(font.render("pos_theta: {} rad".format(round(pos_theta,3)), True, black), (screen_width - 150, y_text_pos))
     y_text_pos+=10
-    screen.blit(font.render("v_l: {} m/s".format(speed_l), True, black), (width - 150, y_text_pos))
+    screen.blit(font.render("v_l: {} m/s".format(speed_l), True, black), (screen_width - 150, y_text_pos))
     y_text_pos+=10
-    screen.blit(font.render("v_r: {} m/s".format(speed_r), True, black), (width - 150, y_text_pos))
+    screen.blit(font.render("v_r: {} m/s".format(speed_r), True, black), (screen_width - 150, y_text_pos))
     y_text_pos+=10
-    screen.blit(font.render("pwm_l: {}".format(pwm_l), True, black), (width - 150, y_text_pos))
+    screen.blit(font.render("pwm_l: {}".format(pwm_l), True, black), (screen_width - 150, y_text_pos))
     y_text_pos+=10
-    screen.blit(font.render("pwm_r: {}".format(pwm_r), True, black), (width - 150, y_text_pos))
+    screen.blit(font.render("pwm_r: {}".format(pwm_r), True, black), (screen_width - 150, y_text_pos))
     y_text_pos+=10
-    screen.blit(font.render("icc_omega: {} rad/s".format(round(icc_omega,3)), True, black), (width - 150, y_text_pos))
+    screen.blit(font.render("icc_omega: {} rad/s".format(round(icc_omega,3)), True, black), (screen_width - 150, y_text_pos))
     y_text_pos+=10
-    screen.blit(font.render("icc_r: {} m".format(round(icc_r,3)), True, black), (width - 150, y_text_pos))
+    screen.blit(font.render("icc_r: {} m".format(round(icc_r,3)), True, black), (screen_width - 150, y_text_pos))
     
     
 
 
 def _draw_bot():
-    global pos_x, pos_y, pos_theta, wdith, height
+    global pos_x, pos_y, pos_theta, wdith, screen_height, icc_r
     color = (200,200,200)
     radius = 5
-    ratio = 10.0
-    pix_x = int(pos_x * ratio+ width / 2.0)
-    pix_y = int(height / 2.0 - pos_y * ratio)
+    ratio = 20.0
+    pix_x = int(pos_x * ratio+ screen_width / 2.0)
+    pix_y = int(screen_height / 2.0 - pos_y * ratio)
     pygame.draw.circle(screen, color, (pix_x,pix_y), radius*2)
     pygame.draw.line(screen, (200, 0, 0), (pix_x, pix_y), (pix_x+math.cos(pos_theta)*(radius+1), pix_y-math.sin(pos_theta)*(radius+1)))
+    
+    #trajectory
+    traj_color =  (50, 200, 50)
+    if icc_r > 100 and (speed_l > 0 or speed_r > 0):
+        pygame.draw.line(screen, traj_color, (pix_x+math.cos(pos_theta)*(radius+3), pix_y-math.sin(pos_theta)*(radius+3)),(pix_x+math.cos(pos_theta)*(radius+1000), pix_y-math.sin(pos_theta)*(radius+1000)))
+    
+    if abs(icc_r * ratio) > 1 and abs(icc_r) < 100:
+        pygame.draw.circle(screen, traj_color, (int(pix_x - math.sin(pos_theta)*(icc_r*ratio)), int(pix_y-math.cos(pos_theta)*(icc_r*ratio))), int(abs(icc_r*ratio)), 1)
 
 
 def update_world():
